4c7ee0fac82083ec52f20a837b4045f1d01e470d
Stephen Ritchie <stephenfritchie@gmail.com>
2017-06-07 14:17:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 # Date: 06/07/2017
 # Version: Pre-Alpha 1.0
 
+import json
+from pprint import pprint
+
 
 
 
@@ -9,6 +12,12 @@
 
 def main():
     exitFlag = False
+    
+    # Importing item list
+    with open('/packages/items.json') as data_file:
+        items = json.load(data_file)
+        
+    pprint(items)
     
     # Main program loop
     while (!exitFlag):
