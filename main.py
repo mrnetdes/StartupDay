5f09ac0fc7db351299a3e2d0bd43c299e7d044d9
recklesspanda <stephenfritchie@gmail.com>
2017-06-06 23:14:01 -0400

    adding basic files

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.insert(0, "/Users/Stephen/Github/StartupDay/packages")
+from items import *
+
+
+
+
+
+def main():
+    print(yearbook.price)
+
+
+main()
---
+++ packages/items.py
@@ -0,0 +1,7 @@
+class yearbook:
+    name = "Yearbook"
+    price = 10
+
+class lanyard:
+    name = "Lanyard"
+    price = 10
