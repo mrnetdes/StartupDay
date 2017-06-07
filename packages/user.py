67b58c17f59a5ffea7c2fd5652bceb2957f4dcbc
Stephen Ritchie <stephenfritchie@gmail.com>
2017-06-07 12:10:03 -0400

    Update user.py

--- packages/user.py
+++ packages/user.py
@@ -7,9 +7,11 @@ class User(object):
         lname: a string representing the user's last name
         propername: a string with the user's propername, which is usually their legal name
         year: an integer representing the user's graduation year
+        
+        items: a dictionary that holds the item name as a string in the key, and the units of
+               item being purchased as an integer in the value
     """
-    
-    def __init__(self):
+    def __init__(self, userid=, fname, lname, propername, year):
         # Personal information
         self.userid = "99999"
         self.fname = "fname"
@@ -17,7 +19,11 @@ class User(object):
         self.propername = "propername"
         self.year = "2014"
         
+        # Inventory information (key=item name, value=units being purchased)
+        self.items = {'_yearbook':0, '_lanyard':0, '_pacdues':0, '_spiritlife':0, '_other':0}
         
+     
+
     def get_userid(self):
         return self.userid
     def set_userid(self, id):
