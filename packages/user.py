8d970f4df3794055062e014ef733c5ffbf80bffa
Stephen Ritchie <stephenfritchie@gmail.com>
2017-06-07 14:06:02 -0400

    Update user.py

--- packages/user.py
+++ packages/user.py
@@ -2,7 +2,7 @@ class User(object):
     """Users have the following properties:
     
     Attributes:
-        userid: an integer holding a unique 5 digit number that corresponds to the database
+        userid: a string holding a unique 5 digit number that corresponds to the database
         fname: a string representing the user's first name
         lname: a string representing the user's last name
         propername: a string with the user's propername, which is usually their legal name
@@ -11,13 +11,13 @@ class User(object):
         items: a dictionary that holds the item name as a string in the key, and the units of
                item being purchased as an integer in the value
     """
-    def __init__(self, userid=, fname, lname, propername, year):
+    def __init__(self, userid, fname, lname, propername, year):
         # Personal information
-        self.userid = "99999"
-        self.fname = "fname"
-        self.lname = "lname"
-        self.propername = "propername"
-        self.year = "2014"
+        self.userid = userid
+        self.fname = fname
+        self.lname = lname
+        self.propername = propername
+        self.year = year
         
         # Inventory information (key=item name, value=units being purchased)
         self.items = {'_yearbook':0, '_lanyard':0, '_pacdues':0, '_spiritlife':0, '_other':0}
