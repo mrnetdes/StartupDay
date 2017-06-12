0e06df808736fd91fb62775f597eac7885ef4293
Stephen Ritchie <stephenfritchie@gmail.com>
2017-06-12 09:37:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,23 +11,47 @@ from pprint import pprint
 
 
 def main():
-    exitFlag = False
+    exitFlag = False # boolean to control main exiting of main program loop
     
     # Importing item list
     with open('/packages/items.json') as data_file:
         items = json.load(data_file)
-        
     pprint(items)
+     
+    #header.title()
+    
+    # Getting a valid operator id
+    userInput = input("Please enter your operator id:")
+    while (!check_operator(userInput)):
+        print("Invalid operator...")
+        userInput = input("Please enter your operator id:)
+    operator_id = userInput
+        
     
     # Main program loop
     while (!exitFlag):
+ 
+        # Getting a valid user id
         userInput = input("Please scan an ID number")
+        while(!check_id(userInput):
+              print("Invalid ID...")
+              userInput = input("Please scan an ID number:")
+              
+              
     
     
     
-    
+#--------------------------------------------------------------------------------------    
 def cleanShutdown():
-    print("Performing a clean shutdown...")
+    exit("Performing a clean shutdown...")
+  
+# check for exit command              
+def check_id(id):
+    return True
+
+# check for exit command
+def check_operator(id):
+    return True
     
 
 
