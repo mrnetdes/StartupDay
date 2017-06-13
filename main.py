32c5335c110f1fc7e16c4873a02254a7a5215726
recklesspanda <stephenfritchie@gmail.com>
2017-06-13 06:55:10 -0400

    update

--- main.py
+++ main.py
@@ -6,70 +6,84 @@ import json
 from pprint import pprint
 
 
-
+DEBUGGING = True
 
 
 
 def main():
     exitFlag = False # boolean to control main exiting of main program loop
-    
+
     # Importing item list
-    with open('/packages/items.json') as data_file:
+    with open('packages/items.json') as data_file:
         items = json.load(data_file)
-    pprint(items)
-     
-        
+    #pprint(items) # this doesn't work correctly yet
+
+
     #header.title()
-    
-    
+
+
     # Getting a valid operator id
-    userInput = input("Please enter your operator id:")
-    while (!check_operator(userInput)):
-        userInput = input("INVALID ID: Please enter your operator id:)
-    operator_id = userInput
-        
-    
+
+
+
     # Main program loop
-    while (!exitFlag):
- 
+    while (exitFlag == False):
+
         # Getting a valid user id
-        userInput = input("Please scan an ID number")
-        while(!check_id(userInput):
-              userInput = input("INVALID ID: Please scan an ID number:")
-              
+        if (DEBUGGING): print(bcolors.HEADER + "\n--Getting a valid user id--" + bcolors.ENDC)
+        userInput = get_id("Please enter id: ")
+
         # Creating user
-              
+
         # Adding items to transaction/adding new user to transaction
-              
+
         # Determining payment methods
-        print("WARNING: a 3% fee will be applied to credit card purchases")
-        split_count = input("How many ways is this transaction being split?") # this needs validation
+        if (DEBUGGING): print(bcolors.HEADER + "\n--Determining payment methods--" + bcolors.ENDC)
+        print(bcolors.WARNING + "WARNING: a 3% fee will be applied to credit card purchases!" + bcolors.ENDC)
+        split_count = input("How many ways is this transaction being split?: ") # this needs validation
         for x in range (1, split_count+1):
-              print("Payment number ", x
-              payment_type = get_payment_type()
-              payment_amount = get_payment_amount()
-                    
-                    
+              print("\tPayment number " + str(x))
+              #payment_type = get_payment_type()
+              #payment_amount = get_payment_amount()
+
+
         # Totaling up all items
+        if (DEBUGGING): print(bcolors.HEADER + "\n--Totaling up all items--" + bcolors.ENDC)
         # include 3% charge for cc
-              
+
         # Creating Receipt
-              
-              
+        if (DEBUGGING): print(bcolors.HEADER + "\n--Creating receipt--" + bcolors.ENDC)
+
+
         # Storing Receipt Locally and in Database
-              
-              
+        if (DEBUGGING): print(bcolors.HEADER + "\n--Storing receipt locally and in database--" + bcolors.ENDC)
+
+
         # Printing Receipt
-              
-              
-    
-    
-    
-#--------------------------------------------------------------------------------------    
-def cleanShutdown():
-    
-  
-# check for exit command              
+        if (DEBUGGING): print(bcolors.HEADER + "\n--Printing receipt--" + bcolors.ENDC)
+
+
+
+
+
+#--------------------------------------------------------------------------------------
+#def cleanShutdown():
+
+# purpose:
+# precondition:
+# postcondition:
+def get_id(prompt):
+    userInput = input(prompt)
+    while True:
+        if (userInput != 999):
+            userInput = input(bcolors.FAIL + "INVALID ID: " + prompt + bcolors.ENDC)
+        else:
+            break
+    return userInput
+
+
+
+# check for exit command
 def check_for_exit(id):
     if (id == "exit"):
         exit("Performing a clean shutdown...")
@@ -80,17 +94,17 @@ def check_operator(id):
     return True
 
 # These functions need validation
-def get_payment_type():
-    userInput = input("Type:")
-    while (userInput != "cc" || userInput != "cash" || userInput != "check"):
-        userInput = input("INVALID: Type:")
-    return userInput
-def get_payment_amount():
-    userInput = input("Amount:")
-    while (userInput < 0):
-        userInput = input("INVALID: Amount:")
-    return userInput
-    
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 
 main()
