c2f73927c9f7c96dd5b32d48b076e4d9294a5478
Stephen Ritchie <stephenfritchie@gmail.com>
2017-06-13 13:12:50 -0400

    Create header.py

---
+++ packages/header.py
@@ -0,0 +1,7 @@
+import bcolors
+
+title():
+    print("TITLE")
+    
+transaction():
+    print(bcolors.HEADER + "------------------ Transaction " + transaction_number + " ------------------" + bcolors.ENDC)
