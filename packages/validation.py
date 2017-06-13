12e163fd8343f5d4bc11f28f0c346c3b2771ab5d
Stephen Ritchie <stephenfritchie@gmail.com>
2017-06-13 13:33:44 -0400

    Added get_id()

--- packages/validation.py
+++ packages/validation.py
@@ -8,12 +8,12 @@ class validation:
             try:
                 userInput = str(input(prompt))
             except ValueError:
-                print(bcolors.FAIL + "INVALID INPUT " + prompt + bcolors.ENDC)
+                print(bcolors.FAIL + "INVALID INPUT " + bcolors.ENDC + prompt)
                 continue
                 
             # Custom validation for cash, check, or credit card   
             if (userInput != "cash" or userInput != "check" or userInput != "creditcard"):
-                print(bcolors.FAIL + "INVALID INPUT " + prompt + bcolors.ENDC)
+                print(bcolors.FAIL + "INVALID INPUT " + bcolors.ENDC + prompt)
                 continue
             else:
                 break
@@ -30,12 +30,12 @@ class validation:
             try:
                 userInput = float(input(prompt))
             except ValueError:
-                print(bcolors.FAIL + "INVALID INPUT " + prompt + bcolors.ENDC)
+                print(bcolors.FAIL + "INVALID PAYMENT TYPE " + bcolors.ENDC + prompt)
                 continue
                 
             # Custom validation minimum amount...NEED TO SEE WHAT MIN AND MAX SHOULD BE
             if (userInput < 0):
-                print(bcolors.FAIL + "INVALID INPUT " + prompt + bcolors.ENDC)
+                print(bcolors.FAIL + "INVALID AMOUNT " + bcolors.ENDC + prompt)
                 continue
             else:
                 break
@@ -52,16 +52,39 @@ class validation:
             try:
                 userInput = str(input(prompt))
             except ValueError:
-                print(bcolors.FAIL + "INVALID INPUT " + prompt + bcolors.ENDC)
+                print(bcolors.FAIL + "INVALID INPUT " + bcolors.ENDC+ prompt)
                 continue
                 
             # Custom validation for proper operator ID
             if (userInput != "SR"):
-                print(bcolors.FAIL + "INVALID INPUT " + prompt + bcolors.ENDC)
+                print(bcolors.FAIL + "OPERATOR ID NOT FOUND " + bcolors.ENDC + prompt)
                 continue
             else:
                 break
                 
         return userInput
+    
+    
+    # purpose:
+    # precondition:
+    # postcondition:
+    def get_id(prompt):
+        while True:
+            # Exception handling for string
+            try:
+                userInput = int(input(prompt))
+            except ValueError:
+                print(bcolors.FAIL + "INVALID INPUT " + bcolors.ENDC + prompt)
+                continue
+                
+            # Custom validation for proper ID number
+            if (userInput != 9):
+                print(bcolors.FAIL + "STUDENT NUMBER NOT FOUND " + bcolors.ENDC + prompt)
+                continue
+            else:
+                break
+                
+        return userInput
+        
                 
   
