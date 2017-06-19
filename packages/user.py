ad55efea416ff741b4cc10b0a78496ec7e715dba
Stephen Ritchie <stephenfritchie@gmail.com>
2017-06-19 12:57:43 -0400

    Update user.py

--- packages/user.py
+++ packages/user.py
@@ -1,6 +1,5 @@
 class User(object):
     """Users have the following properties:
-    
     Attributes:
         userid: a string holding a unique 5 digit number that corresponds to the database
         fname: a string representing the user's first name
@@ -8,8 +7,9 @@ class User(object):
         propername: a string with the user's propername, which is usually their legal name
         year: an integer representing the user's graduation year
         
-        items: a dictionary that holds the item name as a string in the key, and the units of
+        inventory: a dictionary that holds the item name as a string in the key, and the units of
                item being purchased as an integer in the value
+        total: float that will contain the total price of all the items in the User's inventory
     """
     def __init__(self, userid, fname, lname, propername, year):
         # Personal information
@@ -21,16 +21,16 @@ class User(object):
         
         # Inventory information (key=item name, value=units being purchased)
         self.inventory = {'YEARBOOK':0, 'QUARTER_AD':0, 'HALF_ADD':0, 'FULL_AD':0, 'CAFETERIA':0, 'OTHER':0, 'LANYARD':0, 'AGENDA':0, 'PARKING_PASS':0, 'SERVICE_CLUB':0, 'PAC_DUES':0, 'OTHER':0}
+        # Total price of all items in User's inventory sans possible credit card surcharge
         self.total = 0
-        
-     
-
+         
+            
     def get_userid(self):
         return self.userid
     def set_userid(self, id):
         self.userid = id
     
-    # Gets the total of the items in the user's inventory
+    # Gets the total of the items in the user's inventory: NOT YET WORKING
     def get_total(self):
         for key in self.inventory:
             print(key ,"corresponds to ", self.inventory[key])
