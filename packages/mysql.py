5325daef679c1d2a6402a49c7a3bce49d5bb29ef
Stephen Ritchie <stephenfritchie@gmail.com>
2017-06-20 09:53:58 -0400

    Update mysql.py

--- packages/mysql.py
+++ packages/mysql.py
@@ -1,1 +1,22 @@
+import mysql.connector
 
+config = {
+  'user': 'test',
+  'password': 'test',
+  'host': 'test',
+  'database': 'employees'
+}
+
+def mysql_test_connection():
+  try:
+    cnx = mysql.connector.connect(**config)
+  except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+      print("Something is wrong with your username or password")
+    elif erro.errno == errorcode.ER_BAD_DB_ERROR:
+      print("Database does not exist")
+    else:
+      print(err)
+  else:
+    cnx.close()
+    
