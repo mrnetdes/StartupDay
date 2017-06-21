b86ffda832d4b1da604de842ce0e89236fa495ae
Stephen Ritchie <stephenfritchie@gmail.com>
2017-06-21 10:07:00 -0400

    Update and rename fulltest.py to unittest.py

---
+++ tests/unittest.py
@@ -0,0 +1,13 @@
+import unittest
+
+class FullTest(unittest.TestCase):
+  def setUp(self):
+    pass
+  
+  def test_version(self):
+    self.assertTrue(get_version())
+    
+    
+    
+if __name__ == '__main__':
+  unittest.main()
