5b628f1dfd72c1f1926fc5340aa7a860550aa184
Stephen Ritchie <stephenfritchie@gmail.com>
2017-06-21 10:13:56 -0400

    Added get_version()

--- tests/tests.py
+++ tests/tests.py
@@ -1,1 +1,12 @@
+import sys
+
+# Purpose: ensures that Python 2.7 is running on the system
 def get_version():
+  if not sys.version_info[:2] == (2, 7):
+    return False
+  else:
+    return True
+
+# Ensures that an internet connection is present
+def internet_connection():
+  return True
