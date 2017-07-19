f18dfb2da8e1224127dd20e40a137b9d01eb1ed5
Stephen Ritchie <stephenfritchie@gmail.com>
2017-07-19 15:43:17 -0400

    Update mysql.py

--- packages/mysql.py
+++ packages/mysql.py
@@ -19,11 +19,7 @@ class Mysql(object):
         self.host = host
         self.port = port
         self.database = database
-
-    def test_connection(self):
-        """
-        Attempts to connect to the host, ensuring there is a connection
-        """
+        
         config = {
             'user': self.user,
             'password': self.pw,
@@ -31,6 +27,31 @@ class Mysql(object):
             'port': self.port,
             'database': self.database
         }
+        
+    def open_connection(self):
+        try:
+            cnx = mysql.connector.connect(**config)
+            cursor = cnx.cursor()
+            logging.info("MySQL: connection to " + str(self.host) + " was successfull")
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Uh oh :( Please show this message to your IT Administrator")
+                print(str(errorcode.ER_ACCESS_DENIED_ERROR))
+                logging.exception("MySQL: Attempting to connect to " + str(self.host) + ":" + str(errorcode.ER_ACCESS_DENIED_ERROR))
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Uh oh :( Please show this message to your IT Administrator")
+                print(str(errorcode.ER_ACCESS_DENIED_ERROR))
+                logging.exception("MySQL: " + str(errorcode.ER_BAD_DB_ERROR))
+            else:
+                print("Uh oh :( Please show this message to your IT Administrator")
+                print(err)
+                logging.exception("MySQL: " + str(err))
+
+                
+    def test_connection(self):
+        """
+        Attempts to connect to the host, ensuring there is a connection
+        """
         print ("Testing MySQL connection..."),
         try:
             cnx = mysql.connector.connect(**config)
@@ -44,16 +65,14 @@ class Mysql(object):
                 print("FAIL! Database does not exist")
             else:
                 print(err)
+                
+    
+    def close_connection(self):
+        cursor.close()
+        cnx.close()
+        
 
 
 
 
 
-"""--------------------------------------------------------------------------"""
-def main():
-    LCHS = Mysql()
-    LCHS.test_connection()
-
-
-
-main()
